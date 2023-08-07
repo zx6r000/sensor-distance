3565e7cd54bb3946498aa945a3e519d62ca12082
zx6r000 <140099462+zx6r000@users.noreply.github.com>
2023-08-07 17:06:59 +0200

    new2

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
 basic.forever(function () {
-    basic.showString("" + (sonar.ping(
-    DigitalPin.P1,
-    DigitalPin.P1,
-    PingUnit.Centimeters
-    )))
+    basic.showString("" + (pins.analogReadPin(AnalogPin.P1)))
+    led.plotBarGraph(
+    0,
+    0
+    )
+    basic.pause(500)
 })
