2ee04eeafb4f026bb41198a3dcc25cdbe0dd34d6
zx6r000 <140099462+zx6r000@users.noreply.github.com>
2023-08-07 18:02:19 +0200

    finalisation

--- main.ts
+++ main.ts
@@ -1,15 +1,21 @@
 let valeur = 0
 datalogger.setColumnTitles("distance")
+let comp = 0
 basic.forever(function () {
     valeur = sonar.ping(
     DigitalPin.P1,
     DigitalPin.P1,
     PingUnit.Centimeters
     )
-    led.plotBarGraph(
-    Math.map(valeur, 0, 170, 0, 25),
-    0
-    )
-    datalogger.log(datalogger.createCV("distance", valeur))
-    basic.pause(60000)
+    if (comp == 60) {
+        datalogger.log(datalogger.createCV("distance", valeur))
+        comp = 0
+    } else {
+        led.plotBarGraph(
+        Math.map(valeur, 0, 170, 0, 25),
+        0
+        )
+        comp += 1
+    }
+    basic.pause(1000)
 })
